0edb65c569071f45118bb6f588c85987c10038a5
pasqualenaclerio <ing.nacleriopasquale@gmail.com>
2024-02-06 22:07:24 +0100

    second test

---
+++ interrutore_con_stato/interrutore_con_stato.ino
@@ -0,0 +1,30 @@
+int pinLed = 12;      //pin per il led
+int pinSwitch = 11;  //pin per l'interruttore
+bool lettura;        //creo un contenuotre dove mettere la lettura del pulsante
+bool stato = LOW;     //imposto uno stato a basso;
+
+void setup() {
+  pinMode(pinLed, OUTPUT);  //settiamo il pin del led a ingresso
+  pinMode(pinSwitch, INPUT); //settiamo il pin dell'interruttore a uscita
+}
+
+void loop() {
+  lettura = digitalRead(pinSwitch);  //leggiamo il valore del pin 11 e salviamolo
+
+  if(lettura == HIGH){                    //SE la lettura è alta
+    stato = !stato;                       //cambia lo stato
+    delay(200);                           //aspetta qualche secondo
+  }
+
+  if(stato == HIGH){                    //SE lo stato è alta
+    
+      digitalWrite(pinLed, HIGH);        //accendi il led
+      
+    }
+    else{                                //ALTRIMENTI
+      
+      digitalWrite(pinLed, LOW);         //tieni spento il led
+      
+      }
+
+}
